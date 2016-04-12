a7f8c614317c3d881c9ad6f1a184f5ab2a6ae24b
Christopher Boedicker <boedickerchristopher@gmail.com>
2016-04-12 16:44:51 -0500

    File Server Migration
    Used to migrate files from legacy servers to SharePoint Online.

---
+++ Migration/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.SharePoint.Client;
+using System.Security;
+
+
+namespace Migration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            ClientContext context = new ClientContext("https://tshstl.sharepoint.com/");
+
+            {
+                //SharePoint Credentials
+
+                SecureString password = new SecureString();
+                foreach (char c in "Password")
+                    password.AppendChar(c);
+
+                context.Credentials = new SharePointOnlineCredentials("UserName", password);
+
+               
+                context.ExecuteQuery();
+
+                Console.WriteLine("File Transfer is Complete");
+            }
+
+            string[] fileEntries = Directory.GetFiles(@"C:\  ");
+
+
+            foreach (string fileName in fileEntries)
+            {
+                Console.WriteLine(fileName);
+                Console.WriteLine(Path.GetFileName(fileName));
+
+                using (FileStream fs = new FileStream(fileName, FileMode.Open))
+                {
+                    string destination = "/sites/NameofWebsite/documentlibrary/" + Path.GetFileName(fileName);
+
+                    Microsoft.SharePoint.Client.File.SaveBinaryDirect(context, destination, fs, true);  
+
+
+
+                }
+
+
+
+
+
+
+            }
+        }
+    }
+}
+
